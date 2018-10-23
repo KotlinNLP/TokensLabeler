d0a813e4ec050836f6168b06c2729c072aab97f8
KotlinNLP Authors <github@kotlinnlp.com>
2018-10-23 15:05:41 +0200

    Update documentation

--- src/main/kotlin/com/kotlinnlp/tokenslabeler/helpers/LabelStatistics.kt
+++ src/main/kotlin/com/kotlinnlp/tokenslabeler/helpers/LabelStatistics.kt
@@ -1,7 +1,14 @@
+/* Copyright 2018-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
 package com.kotlinnlp.tokenslabeler.helpers
 
 /**
- * The statistics of a single [Label].
+ * The statistics of a single Label.
  *
  * @property truePositive number of times where the model correctly predicts the positive class
  * @property falsePositive number of times where the model incorrectly predicts the positive class
