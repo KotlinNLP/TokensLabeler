347ec622e328fe7c7d5b26dc0170e41351bbc081
KotlinNLP Authors <github@kotlinnlp.com>
2020-02-20 08:50:43 +0100

    Rename package

--- examples/labeling/TextLabeling.kt
+++ examples/labeling/TextLabeling.kt
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  * ------------------------------------------------------------------*/
 
-package text
+package labeling
 
 import com.kotlinnlp.linguisticdescription.sentence.RealSentence
 import com.kotlinnlp.linguisticdescription.sentence.token.RealToken
